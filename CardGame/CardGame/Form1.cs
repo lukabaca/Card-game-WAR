fdc6fa2eb07fa602e5a0db557b93bfa0adf1b2a9
lukabaca <lkszblaszczyk@gmail.com>
2018-06-18 21:38:16 +0200

    init project

---
+++ CardGame/CardGame/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using gameSpace;
+using System.Collections.Generic;
+using System;
+
+namespace CardGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void loadMyCurrentDeck(List<String> currentDeck)
+        {
+
+            for (int i = 0; i < currentDeck.Count; i++)
+            {
+                currentDeck.Add(i.ToString());
+            }
+
+            listBox1.DataSource = currentDeck;
+        }
+    }
+
+    
+}
